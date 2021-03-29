8873e9b3b873fece1d097e396f7f5a11a22cef17
Zack <62968739+zackedwards@users.noreply.github.com>
2021-03-29 00:26:25 -0400

    Update test.js

--- test/test.js
+++ test/test.js
@@ -20,16 +20,16 @@ describe("GitHub EndPoint Tests", function() {
     it("listBranches returns list branches", async function() {
         
       let user  = await github.getUser();
-      let repos = await github.listBranches(user,"HW4-345");
-      expect(repos).to.be.an('array').that.have.nested.property("[0].name").equals("master");
+      let repos = await github.listBranches(user,"REST_API_hw4");
+      expect(repos).to.be.an('array').that.have.nested.property("[0].name").equals("main");
 
     });
 
     it("createRepo successfully creates repo", async function() {
         
       let user  = await github.getUser();
-      let status = await github.createRepo(user, "test-HW4-345");
-      expect(status).to.equal(201);
+      let status = await github.createRepo(user, "REST_API_hw4");
+      expect(status).to.equal(422);
 
     });
 
@@ -37,7 +37,7 @@ describe("GitHub EndPoint Tests", function() {
     it("createIssue successfully creates issue", async function() {
       
       let user  = await github.getUser();
-      let status = await github.createIssue(user, "HW4-345", "issue name", "issue body");
+      let status = await github.createIssue(user, "REST_API_hw4", "issue name", "issue body");
       expect(status).to.equal(201);
 
     });
@@ -45,7 +45,7 @@ describe("GitHub EndPoint Tests", function() {
     it("enableWikiSupport successfully enables wiki support", async function() {
       
       let user  = await github.getUser();
-      let response = await github.enableWikiSupport(user, "HW4-345");
+      let response = await github.enableWikiSupport(user, "ssw345-rest-lab-server");
 
       expect(response).to.have.property('has_wiki');
       expect(response.has_wiki).to.equal(true);
